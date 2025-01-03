4459c67c21efcce5293caefd0178cd2de724b97e
Roland Rusch <roland@rusch.lu>
2025-01-03 14:04:44 +0100

    #3 Create USB host for serial adapter

--- examples/nucleo-f429zi_usbx_host/AZURE_RTOS/App/app_azure_rtos.c
+++ examples/nucleo-f429zi_usbx_host/AZURE_RTOS/App/app_azure_rtos.c
@@ -49,6 +49,11 @@
 static UCHAR tx_byte_pool_buffer[TX_APP_MEM_POOL_SIZE];
 static TX_BYTE_POOL tx_app_byte_pool;
 
+/* USER CODE BEGIN UX_HOST_Pool_Buffer */
+/* USER CODE END UX_HOST_Pool_Buffer */
+static UCHAR  ux_host_byte_pool_buffer[UX_HOST_APP_MEM_POOL_SIZE];
+static TX_BYTE_POOL ux_host_app_byte_pool;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -96,6 +101,32 @@ VOID tx_application_define(VOID *first_unused_memory)
 
     /* USER CODE END  App_ThreadX_Init_Success */
 
+  }
+
+  if (tx_byte_pool_create(&ux_host_app_byte_pool, "Ux App memory pool", ux_host_byte_pool_buffer, UX_HOST_APP_MEM_POOL_SIZE) != TX_SUCCESS)
+  {
+    /* USER CODE BEGIN TX_Byte_Pool_Error */
+
+    /* USER CODE END TX_Byte_Pool_Error */
+  }
+  else
+  {
+    /* USER CODE BEGIN UX_HOST_Byte_Pool_Success */
+
+    /* USER CODE END UX_HOST_Byte_Pool_Success */
+
+    memory_ptr = (VOID *)&ux_host_app_byte_pool;
+
+    if (MX_USBX_Host_Init(memory_ptr) != UX_SUCCESS)
+    {
+      /* USER CODE BEGIN MX_USBX_Host_Init_Error */
+
+      /* USER CODE END MX_USBX_Host_Init_Error */
+    }
+
+    /* USER CODE BEGIN MX_USBX_Host_Init_Success */
+
+    /* USER CODE END MX_USBX_Host_Init_Success */
   }
 
 }
--- examples/nucleo-f429zi_usbx_host/AZURE_RTOS/App/app_azure_rtos.h
+++ examples/nucleo-f429zi_usbx_host/AZURE_RTOS/App/app_azure_rtos.h
@@ -30,6 +30,8 @@ extern "C" {
 #include "app_azure_rtos_config.h"
 
 #include "app_threadx.h"
+
+#include "app_usbx_host.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
