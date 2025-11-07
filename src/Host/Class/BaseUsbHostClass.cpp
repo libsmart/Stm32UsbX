949c25e4ee81b265777db2a1e37ade78d8acfe2e
Roland Rusch <roland@rusch.lu>
2025-11-07 07:34:16 +0100

    #3 Create USB host for serial adapter

--- src/Host/Class/BaseUsbHostClass.cpp
+++ src/Host/Class/BaseUsbHostClass.cpp
@@ -5,25 +5,6 @@
 
 #include "BaseUsbHostClass.hpp"
 
-#if __EXCEPTIONS
-#include <stdexcept>
-#define LIBSMART_HANDLE_ERROR(fmt, ...)                                          \
-do {                                                                    \
-char buffer[snprintf(nullptr, 0, fmt, __VA_ARGS__) + 1]{};              \
-snprintf(buffer, sizeof(buffer), fmt, __VA_ARGS__);                     \
-log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)->println(buffer); \
-throw std::runtime_error(buffer);                                       \
-} while (0);
-#else
-#define LIBSMART_HANDLE_ERROR(fmt, ...)                                          \
-do {                                                                    \
-char buffer[snprintf(nullptr, 0, fmt, __VA_ARGS__) + 1]{};              \
-snprintf(buffer, sizeof(buffer), fmt, __VA_ARGS__);                     \
-log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)->println(buffer); \
-return ret;                                                             \
-} while (0);
-#endif
-
 using namespace Stm32UsbX;
 
 /*
